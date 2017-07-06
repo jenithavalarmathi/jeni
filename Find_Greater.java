1c1e308dd40c7cb4aa7bf71a42375f22f1746bc1
jenithavalarmathi <jenithakulandaivel@gmail.com>
2017-07-06 17:39:48 +0530

    Update and rename Greater.java to Find_Greater.java

---
+++ Find_Greater.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Find_Greater{
+   public static void main(String args[]){
+    int i,j,k;
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter the three values");
+    i=s.nextInt();
+    j=s.nextInt();
+    k=s.nextInt();
+    if(i>=j && i>=k){
+      System.out.println(i+" is greater");
+    }
+    else if(j>=i && j>=k){
+      System.out.println(j+" is greater");
+    }
+    else{
+      System.out.println(k+" is greater");
+    }
+  }
+}
