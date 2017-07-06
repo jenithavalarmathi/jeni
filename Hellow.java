c7712d57f8197a33b24b43c457fe5f84fc43a69c
jenithavalarmathi <jenithakulandaivel@gmail.com>
2017-07-06 18:28:39 +0530

    Create Hellow.java

---
+++ Hellow.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class Hello{
+   public static void main(String args[]){
+   int j;
+   BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+   for(i=0;i<n;i++){
+     
+   System.out.println("Enter the word");
+   i=br.readLine();
+   }
+   }
+}
